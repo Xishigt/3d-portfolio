e65e1ec0e8bdbaaa3cf35f5c6a30b8cb62c96562
Xishigt <XxX231714237@gmail.com>
2024-02-22 02:20:12 +0300

    ajilhq bn

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,6 @@ import './index.css';
 import React from 'react';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode> <App />
+</React.StrictMode>);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,16 @@
 import { Suspense } from 'react'
 
 import { Canvas } from '@react-three/fiber'
-import  Loader  from '@react-three/fiber'
-import { AmbientLight, HemisphereLight, PointLight } from 'three'
 
 import { Island }from '../models/island'
+import Loader from '../components/Loader'
 {/*<section className="w-full h-screen relative">
 <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
     POPUL    
 </div>
 </section>*/}
 const Home = () => {
-    const adjustIslandForScreenSize = () => {
     
-        let screenScale = null;
-        let screenPostion = [0, -0.6, -43];
-        let rotation = [0., 4.7, 0];
-        if(window.innerWidth < 768) {
-            screenScale = [0.9, 0.9, 0.9];
-          }  else {
-            screenScale = [1, 1, 1];
-          }
-           
-            
-        return [screenScale, screenPostion, rotation];
-    }
-    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
-
   return (
    <section className="w-full h-screen relative">
     <section className="w-full h-screen relative">
@@ -43,6 +27,7 @@ const Home = () => {
             <PointLight />
             <SpotLight />
             <HemisphereLight />
+            
 
             <Island />
         </Suspense>
