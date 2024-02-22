5116273bb3ad86c321f7087d82d9776262e6477f
endone <84660082+Davaakhuu02@users.noreply.github.com>
2024-02-22 15:42:50 +0800

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import  Navbar  from './components/Navbar';
+import   Navbar  from './components/Navbar';
 import { Home, About, Projects, Contact  } from './pages';
 
 const App = () => {
